c4dc382327792451045bce6d104d282b985ccc6f
Rey24179 <rey24179@uvg.edu.gt>
2025-03-21 09:39:09 -0600

    javadoc del evualator

--- Evaluator.java
+++ Evaluator.java
@@ -1,5 +1,17 @@
 import java.util.*;
+
+/**
+ * La clase Evaluator evalúa una expresión representada como una lista de cadenas.
+ * La primera cadena representa el operador, y las siguientes son los operandos.
+ */
 class Evaluator {
+    /**
+     * Evalúa una expresión en forma de lista de cadenas.
+     * 
+     * @param expression Lista de cadenas donde el primer elemento es el operador y los siguientes son operandos.
+     * @return El resultado de aplicar el operador a los operandos. Si la lista está vacía, devuelve 0.
+     * @throws NumberFormatException Si algún operando no es un número válido.
+     */
     public static int evaluate(List<String> expression) {
         if (expression.isEmpty()) {
             return 0;
